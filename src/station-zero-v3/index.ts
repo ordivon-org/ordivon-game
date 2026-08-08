1bd780030589b1226d0d9f3e22304a605cfaeaa1
xzh <1666384464@qq.com>
2026-08-08 15:33:17 +0800

    game: issue durable cross-world messages

--- src/station-zero-v3/index.ts
+++ src/station-zero-v3/index.ts
@@ -15,5 +15,6 @@ export * from "./play-projection.ts";
 export * from "./play-service.ts";
 export * from "./reducer.ts";
 export * from "./resource-egress.ts";
+export * from "./message-issuance.ts";
 export * from "./topology.ts";
 export * from "./turn-service.ts";
